14096acee6941bc7c64346d81bd272c0f4a29ab5
RafiqSultan <Rafiqaasultan@gmail.com>
2022-07-17 16:38:44 -0700

    CREATE ecommerce website

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label>(+967)777350652</label>
+            <i className='fa fa-envelope'></i>
+            <label>Rafiq.alsultan@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <label>EN</label>
+            <label>Ar</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
